d6631b5abcdb414436c0a90bc11ba0cd4808eda0
boruoshihao <boruoshihao@gmail.com>
2016-03-02 11:30:17 -0500

    Remove friend function in FFTServer.

--- inc/fftw_hao.h
+++ inc/fftw_hao.h
@@ -5,6 +5,7 @@
 
 class FFTServer
 {
+ public:
     int  dimen;
     int* n;
     int  L;
@@ -14,7 +15,7 @@ class FFTServer
     std::complex<double>* outback;
     fftw_plan planforw;
     fftw_plan planback;
- public:
+
     FFTServer();
     FFTServer(int Dc, const int* Nc, char format); //'C' Column-major: fortran style; 'R' Row-major: c style;
     FFTServer(const FFTServer& x);
@@ -24,11 +25,6 @@ class FFTServer
    
     std::complex<double>* fourier_forw(const std::complex<double>* inarray);
     std::complex<double>* fourier_back(const std::complex<double>* inarray);
-   
-    friend void FFTServer_void_construction_test();
-    friend void FFTServer_param_construction_test();
-    friend void FFTServer_equal_construction_test();
-    friend void FFTServer_equal_test();
 };
 
 #endif
